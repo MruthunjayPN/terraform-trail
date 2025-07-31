e8a44941b5e6611df2ee5d5cf7649076fd6bf751
MruthunjayPN <mruthunjaymuttu@gmail.com>
2025-07-31 19:41:05 +0530

    added eip , nat

--- tf-vpc-basic/main.tf
+++ tf-vpc-basic/main.tf
@@ -66,3 +66,36 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "tf-mpn-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id # NAT GW must be in a public subnet
+
+  tags = {
+    Name = "tf-mpn-nat-gateway"
+  }
+}
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "tf-mpn-private-rt"
+  }
+}
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+}
+

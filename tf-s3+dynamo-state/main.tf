e86a95e29d9eaf76fce5af5a63aeecd939ad6693
MruthunjayPN <mruthunjaymuttu@gmail.com>
2025-07-31 16:36:36 +0530

    created s3+dynamodb to store state

---
+++ tf-s3+dynamo-state/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "tf-mpn-state-storage"
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "tf-mpn-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Dev"
+  }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.tf_state.id
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.tf_lock.name
+}
+
